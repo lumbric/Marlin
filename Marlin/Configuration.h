4c6a576cf8744669e3f01b7b155a13efb5ae82ab
petres <peter.reschenhofer@gmail.com>
2015-05-17 18:05:42 +0200

    without AUTO_BED_LEVELING_GRID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -399,7 +399,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 //    Probe 3 arbitrary points on the bed (that aren't colinear)
 //    You must specify the X & Y coordinates of all 3 points
 
-  #define AUTO_BED_LEVELING_GRID
+  //#define AUTO_BED_LEVELING_GRID
   // with AUTO_BED_LEVELING_GRID, the bed is sampled in a
   // AUTO_BED_LEVELING_GRID_POINTSxAUTO_BED_LEVELING_GRID_POINTS grid
   // and least squares solution is calculated
