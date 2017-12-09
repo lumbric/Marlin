c1bbfb49cf23b776d1db84485963eb391e64edaa
lumbric <lumbric@gmail.com>
2017-12-09 17:15:34 +0100

    Fix printable radius size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -497,7 +497,7 @@
 
   // Print surface diameter/2 minus unreachable space (avoid collisions with vertical towers).
   // Petreg: note: this has been set to 95.0 in original config.
-  #define DELTA_PRINTABLE_RADIUS 90.0 // mm
+  #define DELTA_PRINTABLE_RADIUS 80.0 // mm
 
   // Center-to-center distance of the holes in the diagonal push rods.
   #define DELTA_DIAGONAL_ROD 214.0 // mm
